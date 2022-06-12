a9adb57e40d5a355992245175784f5f598e612aa
gustavobruder <gugabruder@gmail.com>
2022-06-11 21:20:40 -0300

    Adicionado setup trabalho 3

---
+++ src/trabalho3/MapaDispersao.java
@@ -0,0 +1,34 @@
+package trabalho3;
+
+/*
+ * @autores: Gustavo Baroni Bruder, Felipe Melio Tomelin e Ana Carolina da Silva
+ * */
+public class MapaDispersao<K, T> {
+
+    // utilizar interface 'Lista' criada nas aulas
+    private Lista tabela;
+
+    public MapaDispersao(int quantidade) {
+
+    }
+
+    private int calcularHash(K chave) {
+        return 0;
+    }
+
+    public boolean inserir(K chave, T dado) {
+        return false;
+    }
+
+    public T remover(K chave) {
+        return null;
+    }
+
+    public T buscar(K chave) {
+        return null;
+    }
+
+    public int quantosElementos() {
+        return 0;
+    }
+}
